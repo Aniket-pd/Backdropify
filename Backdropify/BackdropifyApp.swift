619ec964dabfffcd4b523de2c25335decbfb66e6
Aniket-pd <aniketprasad123@gmail.com>
2025-04-05 03:02:32 +0530

    Initial Commit

---
+++ Backdropify/BackdropifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackdropifyApp.swift
+//  Backdropify
+//
+//  Created by Aniket prasad on 5/4/25.
+//
+
+import SwiftUI
+
+@main
+struct BackdropifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

188115a39d2d1c8011486a9bfad8db3f4306f280
Aniket-pd <aniketprasad123@gmail.com>
2025-04-08 12:37:35 +0530

    [Features] Configure Firebase in  App Code

--- BackdropifyApp.swift
+++ BackdropifyApp.swift
@@ -6,9 +6,13 @@
 //
 
 import SwiftUI
-
+import Firebase
 @main
 struct BackdropifyApp: App {
+    init(){
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             homeView()

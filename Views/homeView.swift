227cc8064c5f2839d84e353a20400b1a5a14fc2b
Aniket-pd <aniketprasad123@gmail.com>
2025-04-17 03:46:16 +0530

    [Features UI] showcase images cover screen size of iphone

--- Views/homeView.swift
+++ Views/homeView.swift
@@ -1,17 +1,30 @@
 import SwiftUI
 
 struct HomeView: View {
-    var body: some View {
+    @State private var currentIndex = 0
+    private let timer = Timer.publish(every: 5, on: .main, in: .common).autoconnect()
+    private let imageNames = [
+        "Afuturisticneonlitstreeta_18295513",
         
-        ScrollView{
-            ZStack{
-                Image("Afuturisticneonlitstreeta_18295513")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .clipped()
-                  
-                
-                
+    ]
+    
+    var body: some View {
+        ScrollView {
+            TabView(selection: $currentIndex) {
+                ForEach(0..<imageNames.count, id: \.self) { index in
+                    Image(imageNames[index])
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .clipped()
+                        .tag(index)
+                }
+            }
+            .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 0.69) // 30% of screen height
+            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
+            .onReceive(timer) { _ in
+                withAnimation {
+                    currentIndex = (currentIndex + 1) % imageNames.count
+                }
             }
         }
         .ignoresSafeArea()
@@ -20,6 +33,4 @@ struct HomeView: View {
 
 #Preview {
     HomeView()
-    
-        
 }
